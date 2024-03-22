7ad418666dd93c1efcc45b87716d11e6e506e0ff
TushShukla267 <129202888+TushShukla267@users.noreply.github.com>
2024-03-23 02:26:47 +0530

    Started Work on News React App

---
+++ newsapp/src/App.js
@@ -0,0 +1,39 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  t='Tushar J Shukla';
+  render() {
+    return (
+      <div>
+        Hi this is My first class based Component made by , {this.t}
+      </div>
+    )
+  }
+}
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
